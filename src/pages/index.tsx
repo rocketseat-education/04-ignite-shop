65d18dc56523aa9ae402826885035cf392bee1b7
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-08-30 17:38:54 -0300

    Produto & Checkout - Navegação via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/future/image"
 import { GetStaticProps } from "next"
+import Link from "next/link"
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -30,14 +31,18 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product
+              className="keen-slider__slide"
+            >
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -43,7 +43,8 @@ export const Product = styled('a', {
     transition: 'all 0.2s ease-in-out',
 
     strong: {
-      fontSize: '$lg'
+      fontSize: '$lg',
+      color: '$gray100',
     },
 
     span: {
