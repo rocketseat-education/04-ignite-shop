331d704cf22279c8492b5d52047cf1ef1dc832fc
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-08-30 17:32:01 -0300

    Integração com Stripe - Formatando preço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,7 +57,10 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100),
     }
   })
 
