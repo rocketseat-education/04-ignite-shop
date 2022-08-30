a06358d9d761af6b54ca269e722d2015b3f137b5
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-08-30 15:35:48 -0300

    Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
+
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  justifyContent: 'center',
+  minHeight: '100vh',
+})
+
+export const Header = styled('header', {
+  padding: '2rem 0',
+  width: '100%',
+  maxWidth: 1180,
+  margin: '0 auto',
+})
