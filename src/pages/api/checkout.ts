1b0e4c0e007edf5f3a30204c9ab005e4bbef5aa9
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-08-30 19:31:03 -0300

    Produto & Checkout - Dados da compra no sucesso

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -12,7 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ error: 'Price not found.' });
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`;
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,17 +1,29 @@
+import { GetServerSideProps } from "next";
+import Image from "next/future/image";
 import Link from "next/link";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
 import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
-export default function Success() {
+interface SuccessProps {
+  costumerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  }
+}
+
+export default function Success({ costumerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada</h1>
 
       <ImageContainer>
-
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
       </ImageContainer>
 
       <p>
-        Uhuul <strong>Diego Fernandes</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+        Uhuul <strong>{costumerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">
@@ -19,4 +31,25 @@ export default function Success() {
       </Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product']
+  });
+
+  const costumerName = session.customer_details.name;
+  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  return {
+    props: {
+      costumerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0]
+      }
+    }
+  }
 }
