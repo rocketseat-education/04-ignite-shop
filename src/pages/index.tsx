d1c2d66d8782e41091822cbc3a6a93bae20a8012
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-08-30 18:25:33 -0300

    Produto & Checkout - Prefetch de links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,10 +31,8 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => {
         return (
-          <Link href={`/product/${product.id}`} key={product.id}>
-            <Product
-              className="keen-slider__slide"
-            >
+          <Link href={`/product/${product.id}`} key={product.id} prefetch={false}>
+            <Product className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt="" />
 
               <footer>
